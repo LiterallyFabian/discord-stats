493e83be530c1ea008c644a4f5d6505661acba5e
Fabian <lindgrenfabian@gmail.com>
2021-10-30 14:51:05 +0200

    Add way to restart program

--- main.py
+++ main.py
@@ -14,32 +14,33 @@ def count_words(line, query):
 
 
 messages_path = input("Messages path (messages/): ") or "messages"
-query = input("Word to count (leave blank for all messages): ").lower()
-all_messages = []
+while(True):
+    query = input("\nWord to count (leave blank for all messages): ").lower()
+    all_messages = []
 
-# parse the index file
-index = open(os.path.join(messages_path, "index.json"))
-user_data = json.load(index)
+    # parse the index file
+    index = open(os.path.join(messages_path, "index.json"))
+    user_data = json.load(index)
 
+    for message_dir in os.listdir(messages_path):
+        # open message file
+        if(message_dir != "index.json"):
+            user = User(message_dir, user_data[message_dir[1:]])
+            message = Message(user, query)
+            with open(os.path.join(messages_path, message_dir, "messages.csv"), encoding="utf8") as file:
+                # Ignore first line, could be replaced with .pop or something but whatever
+                first_line = True
+                for unparsed_line in file:
+                    if not first_line:
+                        line = get_message(unparsed_line.rstrip())
+                        message.count += count_words(line, query)
+                    else:
+                        first_line = False
 
-for message_dir in os.listdir(messages_path):
-    # open message file
-    if(message_dir != "index.json"):
-        user = User(message_dir, user_data[message_dir[1:]])
-        message = Message(user, query)
-        with open(os.path.join(messages_path, message_dir, "messages.csv"), encoding="utf8") as file:
-            # Ignore first line, could be replaced with .pop or something but whatever
-            first_line = True
-            for unparsed_line in file:
-                if not first_line:
-                    line = get_message(unparsed_line.rstrip())
-                    message.count += count_words(line, query)
-                else:
-                    first_line = False
+                all_messages.append(message)
 
-            all_messages.append(message)
+    all_messages.sort(key=lambda x: x.count, reverse=True)
 
-all_messages.sort(key=lambda x: x.count, reverse=True)
-
-for msg in all_messages:
-    print(msg.count, "  ", msg.user.name)
+    for msg in all_messages:
+        if(msg.count > 0):
+            print(msg.count, "  ", msg.user.name)
