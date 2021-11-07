f5a024209061ab4e87473c65ccf87cb4b898fff8
Fabian <lindgrenfabian@gmail.com>
2021-11-07 15:32:41 +0100

    Add timestamps to message objects

--- Message.py
+++ Message.py
@@ -1,5 +1,8 @@
+import datetime
+
 class Message:
     def __init__(self, user, word):
         self.user = user
         self.word = word
         self.count = 0
+        self.timestamp = datetime.datetime.now()
